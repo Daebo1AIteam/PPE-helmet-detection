dacaa23db6763e9a022dbb7fff94813c01e74d63
danbi-baek <danbi-baek@github.com>
2020-12-11 14:43:34 +0900

    api update

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-"""ppe URL Configuration
+"""api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path
+from django.contrib import admin
 from django.contrib import admin
 from django.urls import path
+from webcam.views import video_feed_1
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('video_feed_1/', video_feed_1, name="video-feed-1")
 ]
